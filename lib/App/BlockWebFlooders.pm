011cb4ae89e66b9b4b23b041022419dafebd342b
perlancar (on pc-office) <perlancar@gmail.com>
2017-06-07 18:32:43 +0700

    Initial commit

---
+++ lib/App/BlockWebFlooders.pm
@@ -0,0 +1,13 @@
+package App::BlockWebFlooders;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Block IP addresses of web flooders using iptables
+
+=head1 SYNOPSIS
+
+See the included L<block-web-flooders> script.
+
+=cut
